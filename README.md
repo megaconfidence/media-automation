bae0aedbc988b0a9f535edd14d915059ce9e0b68
megaconfidence <cokoghenun@gmail.com>
2025-01-11 01:11:42 +0000

    feat: Adds dynamic series name to renamer

---
+++ README.md
@@ -0,0 +1,8 @@
+# media-automation
+
+## Using the renamer script
+
+```sh
+chmod +x rename.sh
+./rename.sh <series_name>
+```
--- rename.sh
+++ rename.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# USAGE:
+# ./rename.sh series_name
+
+# Check if the series name argument is provided
+if [[ -z $1 ]]; then
+  echo "Usage: $0 <series_name>"
+  exit 1
+fi
+
+# Store the series name from the first argument
+series_name=$1
+
 # Loop through all files matching the pattern
 for file in EP.*.v*.1080p.mp4; do
   # Extract the episode number using regex
   if [[ $file =~ EP\.([0-9]+)\.v[0-9]+\..* ]]; then
     episode_number=${BASH_REMATCH[1]}
     # Construct the new file name
-    new_file_name="one_piece_E${episode_number}.mp4"
+    new_file_name="${series_name}_E${episode_number}.mp4"
     # Rename the file
     mv "$file" "$new_file_name"
     echo "Renamed '$file' to '$new_file_name'"
