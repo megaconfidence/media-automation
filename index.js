cf520c9826c9cd6e5ebcf3078bf28ec560c63700
megaconfidence <cokoghenun@gmail.com>
2025-01-11 01:23:29 +0000

    feat: Adds dynamic series

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import puppeteer from "puppeteer-core";
 import episodeDownloader from "./episode-downloader.js";
+import commandLineArgs from "command-line-args";
 
 function paginate(array, pageSize, pageNumber) {
   //page number starts from 1
   return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
 }
+
 (async () => {
+  const options = commandLineArgs([
+    { name: "link", alias: "l", type: String, defaultOption: true },
+  ]);
   const browser = await puppeteer.connect({
     headless: false,
     browserURL: "http://localhost:9222",
-    // browserWSEndpoint:
-    // "ws://127.0.0.1:9222/devtools/browser/f1ed9cc2-959f-49ee-896f-e9b4d0217d28",
   });
   const page = await browser.newPage();
 
-  const seriesLink = "https://w7.animeland.tv/category/one-piece";
+  const seriesLink = options.link;
   await page.goto(seriesLink, {
     waitUntil: "domcontentloaded",
   });
@@ -27,16 +30,12 @@ function paginate(array, pageSize, pageNumber) {
   const downloadListSize = 10;
   const numOfDownloadPages = Math.ceil(links.length / downloadListSize);
 
-  // for (let i = 1; i <= numOfDownloadPages; i++) {
-  //   const currentDownloadList = paginate(links, downloadListSize, i);
-  //   console.log({ currentPage });
-  //
-  // }
-  const currentDownloadList = paginate(links, downloadListSize, 1);
-  for (const downloadItem of currentDownloadList) {
-    await episodeDownloader(browser, downloadItem);
+  for (let i = 1; i <= numOfDownloadPages; i++) {
+    const currentDownloadList = paginate(links, downloadListSize, i);
+    for (const downloadItem of currentDownloadList) {
+      await episodeDownloader(browser, downloadItem);
+    }
   }
-
   // await browser.close();
   process.exit();
 })();
